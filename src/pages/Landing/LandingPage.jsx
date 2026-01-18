a46f1a96bc2094476d4a8f79bcf7b22486d9b10c
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-01-18 20:06:06 -0300

    feat(sponsor-section): add base structure for sponsor section
    - Cria a estrutura base da seção de planos de patrocínio.
    - Delimita os principais elementos que compõem a tela conforme o protótipo.
    - Adiciona textos iniciais dos tiers (Member, Family, Official).
    - Inclui o componente na landing page para visualização e validação visual durante o desenvolvimento.
    - Estrutura ainda não refinada, receberá ajustes posteriores.
    
    Ref POC-41

--- src/pages/Landing/LandingPage.jsx
+++ src/pages/Landing/LandingPage.jsx
@@ -1,4 +1,5 @@
 import { Hero } from '../../components/landing/Hero'
+import { SponsorSection } from '../../components/layout/SponsorSection/SponsorSection'
 /* 
 Importação das outras seções da Landing Page:
 
@@ -6,7 +7,6 @@ import { TalkListenSection } from '../../components/landing/TalkListenSection'
 import { WhatListenersSay } from '../../components/landing/WhatListenersSay'
 import { MembershipBenefits } from '../../components/landing/MembershipBenefits'
 import { RecentEpisodesSection } from '../../components/landing/RecentEpisodesSection'
-import { SponsorsSection } from '../../components/landing/SponsorsSection'
 import { ArticlesAndNews } from '../../components/landing/ArticlesAndNews'
 import { AppDownloadSection } from '../../components/landing/AppDownloadSection'
 */
@@ -16,12 +16,14 @@ export function LandingPage() {
     <>
       <main>
         <Hero />
+        <SponsorSection />
+
         {/* 
         <TalkListenSection />
         <WhatListenersSay />
         <MembershipBenefits />
         <RecentEpisodesSection />
-        <SponsorsSection />
+        
         <ArticlesAndNews />
         <AppDownloadSection />
         */}

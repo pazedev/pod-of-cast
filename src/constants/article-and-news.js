80b348d84ac01ea40b8641b1d4be4a1f34d0ea9e
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-02-09 17:56:09 -0300

    chore(front): prepare assets and article data structure for Article and News section
    - Adicionadas as imagens e figuras ilustrativas da section "Article and News" conforme o protótipo do Figma.
    
    - Criado arquivo JavaScript para centralizar os dados dos artigos em uma constante reutilizável.
    
    - Estruturada a constante ARTICLE_AND_NEWS para servir como fonte única de dados da section.
    
    - Preparação da base de conteúdo facilita futuras renderizações e garante manutenção centralizada, sem acoplamento direto à camada de UI.
    
    Refs: POC-69

---
+++ src/constants/article-and-news.js
@@ -0,0 +1,38 @@
+export const ARTICLE_AND_NEWS = [
+  {
+    titulo: 'Setup your own podcast',
+    categoria: 'podcast',
+    resumo:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimvel iam, quis nostrud exercitation ullamco laboris...',
+    data: 'Sep 14, 2021',
+    tags: ['business', 'startup'],
+    imagemCapa: '/src/assets/articles/setup-your-own-podcast.png',
+  },
+  {
+    titulo: 'Doodle artwork 101',
+    categoria: 'TIPS & TRICK',
+    resumo:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimvel iam, quis nostrud exercitation ullamco laboris...',
+    data: 'Sep 12, 2021',
+    tags: ['art', 'creative', 'tips and trick'],
+    imagemCapa: '/src/assets/articles/doodle-artwork-101.png',
+  },
+  {
+    titulo: 'Mother Nature Taking Over',
+    categoria: 'NEWS',
+    resumo:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimvel iam, quis nostrud exercitation ullamco laboris... ',
+    data: 'Sep 10, 2021',
+    tags: ['news', 'nature'],
+    imagemCapa: '/src/assets/articles/mother-nature-taking-over.png',
+  },
+  {
+    titulo: 'How to Be Productive',
+    categoria: 'PRODUCTIVITY',
+    resumo:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimvel iam, quis nostrud exercitation ullamco laboris...',
+    data: 'Sep 10, 2021',
+    tags: ['productivity', 'mindset'],
+    imagemCapa: '/src/assets/articles/how-to-be-productive.png',
+  },
+]

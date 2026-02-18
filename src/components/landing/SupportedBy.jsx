27f5e123fc705990b1e1e4f42cf7a873b5a8b507
nicolyz <nicolysantos992@gmail.com>
2026-02-18 18:48:42 -0300

    feat: add SupportedBy section

---
+++ src/components/landing/SupportedBy.jsx
@@ -0,0 +1,34 @@
+import googlePodcast from '../../assets/medium-google-podcast.png'
+import spotify from '../../assets/medium-spotify.png'
+import youtube from '../../assets/medium-youtube.png'
+
+const sponsors = [
+  { name: 'Spotify', logo: spotify },
+  { name: 'Google Podcast', logo: googlePodcast },
+  { name: 'Youtube', logo: youtube },
+]
+
+export function SupportedBy() {
+  return (
+    <section className="w-full px-10 md:px-20">
+      <div className="max-w-[1160px] mx-auto py-6 border-y-2 border-davys-grey">
+        <div className="flex flex-col items-center gap-4 md:flex-row md:justify-center md:gap-[210px]">
+          <p className="text-md font-semibold font-base whitespace-nowrap">
+            Supported by:
+          </p>
+
+          <div
+            className="flex flex-row items-center gap-[90px]"
+            aria-label="Plataformas que apoiam o podcast"
+          >
+            {sponsors.map(sponsor => (
+              <span key={sponsor.name}>
+                <img src={sponsor.logo} alt={`Logo do ${sponsor.name}`} />
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
